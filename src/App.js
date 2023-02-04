490a6ce5e4f3a1981baaaa3e928967b3701eb191
meetul <meetulrathore98@gmail.com>
2023-02-04 20:13:33 +0530

    add alert

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar2 from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
+import Alert from "./components/layout/Alert";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import { GithubProvider } from "./contexts/github/GithubContext";
+import { AlertProvider } from "./contexts/alert/AlertContext";
 
 function App() {
     return (
         <GithubProvider>
-            <Router>
-                <div className="d-flex flex-column justify-content-between min-vh-100">
-                    <Navbar2 />
-                    <main className="container-fluid fs-4 mx-auto px-3 pb-3">
-                        <Routes>
-                            <Route path="/" element={<Home />}></Route>
-                            <Route path="/about" element={<About />}></Route>
-                            <Route path="/notfound" element={<NotFound />}></Route>
-                            <Route path="/*" element={<NotFound />}></Route>
-                        </Routes>
-                    </main>
-                    <Footer />
-                </div>
-            </Router>
+            <AlertProvider>
+                <Router>
+                    <div className="d-flex flex-column justify-content-between min-vh-100">
+                        <Navbar2 />
+                        <main className="container-fluid fs-4 mt-5 px-3 pb-3">
+                            <Alert />
+                            <Routes>
+                                <Route path="/" element={<Home />}></Route>
+                                <Route path="/about" element={<About />}></Route>
+                                <Route path="/notfound" element={<NotFound />}></Route>
+                                <Route path="/*" element={<NotFound />}></Route>
+                            </Routes>
+                        </main>
+                        <Footer />
+                    </div>
+                </Router>
+            </AlertProvider>
         </GithubProvider>
     );
 }
---
+++ src/components/layout/Alert.jsx
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import AlertContext from "../../contexts/alert/AlertContext";
+
+function Alert() {
+    const { alert } = useContext(AlertContext);
+
+    return alert !== null && (
+        <p className="d-flex container fs-6 align-items-center mb-2">
+            {alert.type === "error" && (
+                <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
+                    viewBox="0 0 50 50" xmlSpace="preserve" width="1rem" height="1rem" className="me-2">
+                    <circle style={{ fill: "#D75A4A" }} cx="25" cy="25" r="25" />
+                    <polyline style={{ fill: "none", stroke: "#FFFFFF", strokeWidth: "2", strokeLinecap: "round", strokeMiterlimit: "10" }} points="16,34 25,25 34,16" />
+                    <polyline style={{ fill: "none", stroke: "#FFFFFF", strokeWidth: "2", strokeLinecap: "round", strokeMiterlimit: "10" }} points="16,16 25,25 34,34" />
+                </svg>
+            )}
+            <strong className="mb-0">{alert.msg}</strong>
+        </p>
+    )
+}
+
+export default Alert
